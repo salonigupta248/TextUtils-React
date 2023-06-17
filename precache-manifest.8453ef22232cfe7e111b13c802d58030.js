a31a8e41c410d12250372e4f09ed7f3323eeb497
Saloni Gupta <salonigupta0234@gmail.com>
2023-06-17 23:46:01 +0530

    Updates

---
+++ precache-manifest.8453ef22232cfe7e111b13c802d58030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "322018492bafa84c03b2",
+    "url": "/TextUtils-React/static/css/main.64669179.chunk.css"
+  },
+  {
+    "revision": "322018492bafa84c03b2",
+    "url": "/TextUtils-React/static/js/main.32201849.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtils-React/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "c2188d25d2d7aa117303",
+    "url": "/TextUtils-React/static/js/2.c2188d25.chunk.js"
+  },
+  {
+    "revision": "d59b444719da3c63ba0b",
+    "url": "/TextUtils-React/static/js/runtime~main.d59b4447.js"
+  },
+  {
+    "revision": "2114c2f587ee8c76fcbae475331e5faa",
+    "url": "/TextUtils-React/index.html"
+  }
+];
